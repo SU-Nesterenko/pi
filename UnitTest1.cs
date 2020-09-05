82bea64422bc8f7e3c575dd3635958ea85b99e04
SU-Nesterenko <55326271+SU-Nesterenko@users.noreply.github.com>
2020-09-05 10:36:21 +0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace SeleniumItself
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver browser;
+        IWebElement tb, btn,lb;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Navigate().GoToUrl("http://localhost:8646/WebForm1.aspx");
+            tb = browser.FindElement(By.Id("TextBox1"));
+            btn = browser.FindElement(By.Id("Button1"));
+           
+        }
+
+        [TestMethod]
+        public void TooSmall()
+        {
+            tb.SendKeys("5");
+            btn.Click();
+            //Thread.Sleep(1000);
+            lb = browser.FindElement(By.Id("Label1"));
+            Assert.AreEqual(lb.Text,"Ты ещё мал");
+        }
+
+        [TestMethod]
+        public void Good()
+        {
+            tb.SendKeys("25");
+            btn.Click();
+            //Thread.Sleep(1000);
+            lb = browser.FindElement(By.Id("Label1"));
+            Assert.AreEqual(lb.Text, "Хорошо");
+        }
+    }
+}
