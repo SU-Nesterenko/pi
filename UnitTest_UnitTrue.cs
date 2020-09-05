5dd8531bd2001333296802073e2d585af7c16e2b
SU-Nesterenko <55326271+SU-Nesterenko@users.noreply.github.com>
2020-09-05 10:37:27 +0500

    Add files via upload

---
+++ UnitTest_UnitTrue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLib.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Divide10on5()
+        {
+            MyCalc myCalc = new MyCalc();
+            double result = myCalc.Divide(10, 5);
+            Assert.AreEqual(result,2);
+        }
+
+        [TestMethod]
+        public void DivideOnZero()
+        {
+            MyCalc myCalc = new MyCalc();
+            try
+            {
+                myCalc.Divide(1,0);
+            }
+            catch(MyException ex)
+            {
+                return;
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail("Неверный тип исключения");
+            }
+
+            Assert.Fail("Исключение не сгенерировано");
+        }
+    }
+}
